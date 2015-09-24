eecc3c8de8d10ebbe7d902cfd9a9d0d6c2b5804f
kinton <kintonk@yandex.ru>
2015-09-24 17:30:14 +0300

    Начало

---
+++ index.php
@@ -0,0 +1,44 @@
+     <!DOCTYPE html>
+    <html>
+      <head>
+        <title>Swaper!</title>
+
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <link href="bootstrap/css/bootstrap.css" rel="stylesheet" media="screen">
+        <link href="bootstrap/css/bootstrap-responsive.css" rel="stylesheet" media="screen">
+        <link href="css/css_main.css" rel="stylesheet" media="screen">
+      </head>
+      <body>
+
+		<div class="container main">
+			
+			<div class="row header">
+				<div class="span2"><div class="logo">Swaper</div></div>
+				<div class="span4 offset6">
+					<div class="entry">
+						<button class="btn btn-primary entry-buttons">Зарегистрироваться</button>
+						<button class="btn btn-success entry-buttons">Войти</button>
+					</div>
+				</div>
+			</div>
+			<div class="row">
+				<div class="span3"></div>
+				<div class="span3"></div>
+				<div class="span3"></div>
+				<div class="span3"></div>
+			</div>
+			<div class="row">
+				<div class="span3"></div>
+				<div class="span3"></div>
+				<div class="span3"></div>
+				<div class="span3"></div>
+			</div>
+
+		</div>
+
+        <script src="http://code.jquery.com/jquery-latest.js"></script>
+        <script src="bootstrap/js/bootstrap.min.js"></script>
+      </body>
+    </html>
